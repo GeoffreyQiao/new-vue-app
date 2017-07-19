da8b2747c6eff2f4865960fb02ff35aab2cd0b92
GeoffreyQiao <qlql0101@gmail.com>
2017-07-19 17:32:06 +0800

    new

---
+++ .postcssrc.js
@@ -0,0 +1,10 @@
+// https://github.com/michael-ciniawsky/postcss-load-config
+
+// @ts-ignore
+module.exports = {
+
+    "plugins": {
+        // to edit target browsers: use "browserslist" field in package.json
+        "autoprefixer": {}
+    }
+}
